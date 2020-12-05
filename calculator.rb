917e8d79aa5aa277745b6f805684391991f851d6
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2020-12-05 19:31:38 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
-first_number = 1 
-second_number = 3
+first_number = 3 
+second_number = 4
 sum = first_number + second_number 
 difference = first_number - second_number
-product = first_number * second_num
+product = first_number * second_number
 
 
