c880c6c3f3b413ed62e3921896e161ffcdacd866
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2020-12-05 19:18:59 +0000

    Automatically backed up by Learn

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -4,11 +4,8 @@ describe "./calculator.rb" do
   
 
   it "contains a local variable called first_number that is assigned to a number" do
-    first_number = 0
     first_number = get_variable_from_file("./calculator.rb", "first_number")
     expect(first_number).to be_an(Integer).or be_a(Float)
-
-
     
   end
 
@@ -21,7 +18,6 @@ describe "./calculator.rb" do
   end
 
   it "contains a local variable called sum that is assigned to the result of adding first_number and second_number" do
-    sum = first_number + second_number
     first_number = get_variable_from_file('./calculator.rb', "first_number")
     second_number = get_variable_from_file('./calculator.rb', "second_number")
     sum = get_variable_from_file('./calculator.rb', "sum")
@@ -39,7 +35,6 @@ describe "./calculator.rb" do
   end
 
   it "contains a local variable called product that is assigned to the result of multiplying first_number and second_number" do
-    product = first_number * second_number
     first_number = get_variable_from_file('./calculator.rb', "first_number")
     second_number = get_variable_from_file('./calculator.rb', "second_number")
     product = get_variable_from_file('./calculator.rb', "product")
@@ -48,7 +43,6 @@ describe "./calculator.rb" do
   end
 
   it "contains a local variable called quotient that is assigned to the result of dividing first_number by second_number" do
-    quotient = first_number / second_number
     first_number = get_variable_from_file('./calculator.rb', "first_number")
     second_number = get_variable_from_file('./calculator.rb', "second_number")
     quotient = get_variable_from_file('./calculator.rb', "quotient")
